38c455f493311b86fb378f40df0c076cdc3eb781
FATİH BAYIR <fatihhusnub@gmail.com>
2023-10-31 14:59:40 +0300

    Güncellendi

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const fn = function (){
-    console.log("Function is  starting")
-}
+const express = require ("express")
+const ejs = require ("ejs");
+
+const path = require ("path")
+
+
+
+const app = express ();
+
+
+
+//MIDDLEWARES
+app.use(express.static('public'))
+
+
+//TEMPLATE EnGINE
+app.set("view engine","ejs");
+
+app.get('/',(req,res)=>{
+   res.sendFile(path.resolve(__dirname,"views/index.html"))
+})
+
+const port = 3001;
+
+app.listen(port,()=>{
+    console.log(`Sunucu ${port} portunda başlatıldı..`)
+})
